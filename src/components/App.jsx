65d4327cd94c374e6362caaabbbd255a6300e6db
hyomithetech <hyomithetech@gmail.com>
2019-11-04 17:21:14 -0800

    Work in process & Having trouble with importing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,8 @@ var App = () => (
   </div>
 );
 
+ReactDOM.render(<App />, document.getElementById('app'));
+
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
