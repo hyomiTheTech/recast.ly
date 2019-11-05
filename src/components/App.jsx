ef7869143c7d22e0bf47db344ab1c67926d28bda
hyomithetech <hyomithetech@gmail.com>
2019-11-04 20:51:48 -0800

    Work in process

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoListEntry from './VideoListEntry.js';
+import VideoPlayer from './VideoPlayer.js';
 
 var App = () => (
   <div>
@@ -11,7 +12,7 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em><VideoPlayer videos={exampleVideoData}/></em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
         <div>
