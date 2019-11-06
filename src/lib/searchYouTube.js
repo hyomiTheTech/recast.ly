ea23bfa34e68d61cb29f491faf70074458590d4c
hyomithetech <hyomithetech@gmail.com>
2019-11-05 22:18:05 -0800

    Make searchYouTube.js accepts key, query, and max options

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,14 +1,16 @@
+import YOUTUBE_API_KEY from '../config/youtube.js';
 
 var searchYouTube = (options, callback) => {
   $.ajax({
-    url: `https://www.youtube.com`,
+    url: 'https://www.googleapis.com/youtube/v3/search',
     type: 'GET',
     data: {
-      query: "",
-      max: 5,
-      key:
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      videoEmbeddable: true
     }
-  })
+  });
 };
 
 export default searchYouTube;
