e9068dc377dcfeb28118a5c4b30904445f594e79
hyomithetech <hyomithetech@gmail.com>
2019-11-07 10:41:05 -0800

    Change the get method

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,16 +1,22 @@
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
 var searchYouTube = (options, callback) => {
-  $.ajax({
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    type: 'GET',
-    data: {
-      q: options.query,
-      maxResults: options.max,
-      key: options.key,
-      videoEmbeddable: true
-    }
-  });
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    part: 'snippet',
+    q: options.query,
+    maxResults: options.max,
+    key: options.key,
+    videoEmbeddable: true,
+    type: 'video',
+  })
+    .done(({items}) => {
+      if (callback) {
+        callback(items);
+      }
+    })
+    .fail(({responseJSON}) =>{
+      responseJSON.error.errors.forEach((err) => console.error(err));
+    });
 };
 
 export default searchYouTube;
